0877ed8422e87b1e3c48f04e1e7b5c1d0029e2e3
GFunction <ryan.mm.taylor@gmail.com>
2024-09-28 00:02:37 -0700

    programs & libs

---
+++ {FourZeroOne}/ProgramSource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#nullable enable
+namespace FourZeroOne
+{
+    public readonly struct ProgramSource
+    {
+        public State StartingState { get; init; }
+        public Token.IToken<Core.Resolutions.Objects.Board.Player> Expression { get; init; }
+    }
+}
