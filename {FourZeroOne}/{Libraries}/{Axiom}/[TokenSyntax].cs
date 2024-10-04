76c1f426b3a375c87b579e4d5d43985bd47c9c5a
GFunction <ryan.mm.taylor@gmail.com>
2024-10-04 01:12:15 -0700

    syntax

--- {FourZeroOne}/{Libraries}/{Axiom}/[TokenSyntax].cs
+++ {FourZeroOne}/{Libraries}/{Axiom}/[TokenSyntax].cs
@@ -4,21 +4,25 @@ using System.Collections.Generic;
 namespace FourZeroOne.Libraries.Axiom.TokenSyntax
 {
     using r = Core.Resolutions;
+    using ro = Core.Resolutions.Objects;
     using t = Core.Tokens;
     using ResObj = Resolution.IResolution;
-    using IToken = Token.Unsafe.IToken;
-
+    using Token;
+    using aEffects = Components.Unit.Effects;
 
     public static class AxiomT
     {
         
-        public static t.Fixed<Components.Unit.Effects.Slow.Component> tEffectSlow()
+        public static t.Fixed<aEffects.Slow.Component> tEffectSlow()
         { return new(new()); }
-        public static t.Fixed<Components.Unit.Effects.Slow.Identifier> tEffectSlowCI()
+        public static t.Fixed<aEffects.Slow.Identifier> tEffectSlowCI()
         { return new(new()); }
     }
     public static class _Extensions
     {
-        
+        public static t.Component.Get<ro.Board.Unit, aEffects.Slow.Identifier, aEffects.Slow.Component> tGetEffectSlow(this IToken<ro.Board.Unit> unit)
+        {
+            return new(unit, AxiomT.tEffectSlowCI());
+        }
     }
 }
