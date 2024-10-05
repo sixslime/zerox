9fbcf193d29f29a1ec12bb5c7b4bbe4476751e27
GFunction <ryan.mm.taylor@gmail.com>
2024-10-05 14:31:30 -0700

    Update [Macros].cs

--- {FourZeroOne}/{Core}/[Macros].cs
+++ {FourZeroOne}/{Core}/[Macros].cs
@@ -15,18 +15,4 @@ namespace FourZeroOne.Core.Macros
     using FourZeroOne.Core.Resolutions.Actions.Board.Unit;
     using ProxySyntax;
 
-    public sealed record Map<RIn, ROut> : OneArg<Resolution.IMulti<RIn>, r.Multi<ROut>>
-        where RIn : class, ResObj
-        where ROut : class, ResObj
-    {
-        
-    }
-
-    public sealed record Anonymous<RArg1, ROut> : OneArg<RArg1, ROut>
-        where RArg1 : class, ResObj
-        where ROut : class, ResObj
-    {
-        public required IProxy<Anonymous<RArg1, ROut>, ROut> Proxy { get; init; }
-        public Anonymous(IToken<RArg1> arg1) : base(arg1) { }
-    }
 }
