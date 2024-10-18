7cfd7c4341d862552dbc136836df8e57f3585587
GFunction <ryan.mm.taylor@gmail.com>
2024-10-17 19:54:32 -0700

    Update Tester.cs

--- Tester.cs
+++ Tester.cs
@@ -2,8 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using FourZeroOne.Core.ProxySyntax;
-using FourZeroOne.Core.TokenSyntax;
+using FourZeroOne.Core.Syntax;
 using t = FourZeroOne.Core.Tokens;
 using p = FourZeroOne.Core.Proxies;
 using r = FourZeroOne.Core.Resolutions;
@@ -27,11 +26,11 @@ public class Tester
         // tutorial on how to make tokens to get you up to speed:
         // (tokens for in-game usage will include resolutions of actual objects/actions, but work exactly the same)
         var token_tutorial_1 = 5.tFixed().tAdd(10.tFixed()); // 5 + 10 
-        var token_tutorial_2 = Iter.Over(1, 2, 3, 4).Map(x => x.tFixed()).tToConstMulti(); // [1, 2, 3, 4]
-        var token_tutorial_3 = token_tutorial_2.tIO_SelectOne(); //prompt user to select one from [1, 2, 3, 4], and return it
-        var token_tutorial_4 = CoreT.tSubEnvironment(RHint<ro.Number>.Hint(), new()
+        var token_tutorial_2 = Iter.Over(1, 2, 3, 4).Map(x => x.tFixed()).t_ToConstMulti(); // [1, 2, 3, 4]
+        var token_tutorial_3 = token_tutorial_2.tIOSelectOne(); //prompt user to select one from [1, 2, 3, 4], and return it
+        var token_tutorial_4 = Core.tSubEnvironment(RHint<ro.Number>.Hint(), new()
         {
-            Environment = token_tutorial_2.tIO_SelectOne().tAsVariable(out var mySelection).tYield(),
+            Environment = token_tutorial_2.tIOSelectOne().tAsVariable(out var mySelection).tYield(),
             SubToken = mySelection.tRef().tMultiply(mySelection.tRef())
         }); //creates a Sub-Environment (aka scope) where 'mySelection' stores the resolution of a user selection, then references it twice to multiply it by itself.
         // is different than just calling 'token_tutorial_2.tIO_SelectOne()' twice, that would prompt the user selection 2 times, possibly resolving different values each time (because the user could select 2 different things obv.).
@@ -39,12 +38,12 @@ public class Tester
         // Rules are expressed by 'Proxies', which are basically just tokens, but have the ability to reference information about the token they are meant to replace (such as arguements).
         // logically, the replaced token and replacing token must both have the same resolution type.
         // MakeProxy.RuleFor<{token type to replace}, {resolution type}>({proxy statement specifying the replacement})
-        var rule_tutorial_1 = CoreP.RuleFor<t.Fixed<ro.Number>, ro.Number>(P => 4.tFixed().pDirect(P)); // makes ALL constant number tokens ('t.Fixed<ro.Number>') turn into 4 (as a constant number token).
-        var rule_tutorial_2 = CoreP.RuleFor<t.Number.Add, ro.Number>(P => P.pOriginalA().pAdd(P.pOriginalA()).pSubtract(P.pOriginalB())); // makes ALL add(A, B) tokens ('t.Number.Add') turn into subtract(add(A, A), B).
+        var rule_tutorial_1 = ProxyStatement.BuildAsRule<t.Fixed<ro.Number>, ro.Number>(P => 4.tFixed().pDirect(P)); // makes ALL constant number tokens ('t.Fixed<ro.Number>') turn into 4 (as a constant number token).
+        var rule_tutorial_2 = ProxyStatement.BuildAsRule<t.Number.Add, ro.Number>(P => P.pOriginalA().pAdd(P.pOriginalA()).pSubtract(P.pOriginalB())); // makes ALL add(A, B) tokens ('t.Number.Add') turn into subtract(add(A, A), B).
         //var rule_illogical = MakeProxy.RuleFor<t.Number.Add, ro.Bool>(P => P.pOriginalA().pIsGreaterThan(P.pOriginalB()) -- consider applying this rule to subtract(add(<number>, <number>), <number>), it would become subtract(<bool>, <number>), which does not make sense.
 
         /*
-        var token_complicated = CoreT.tRecursive<ro.Number, r.Multi<ro.Number>, ro.Number>(new() // if you can figure out what this does, then you understand the language; yes its recursive (recursion is not planned to be common, but it will exist sometimes)
+        var token_complicated = Core.tRecursive<ro.Number, r.Multi<ro.Number>, ro.Number>(new() // if you can figure out what this does, then you understand the language; yes its recursive (recursion is not planned to be common, but it will exist sometimes)
         {
             A = 0.tConst(),
             B = 1.Sequence(x => x + 1).Take(5).Map(x => x.tConst()).tToMulti(),
@@ -69,16 +68,16 @@ public class Tester
                 })
         });
         */
-        var token_complicated = CoreT.tMetaRecursiveFunction(RHint<ro.Number, r.Multi<ro.Number>, ro.Number>.Hint(), (selfFunc, counter, pool) =>
+        var token_complicated = Core.tMetaRecursiveFunction(RHint<ro.Number, r.Multi<ro.Number>, ro.Number>.Hint(), (selfFunc, counter, pool) =>
         {
             return counter.tRef().tIsGreaterThan(2.tFixed()).tIfTrue(RHint<ro.Number>.Hint(), new()
             {
-                Then = CoreT.tMetaFunction(RHint<ro.Number>.Hint(), () => { return 0.tFixed(); }),
-                Else = CoreT.tMetaFunction(RHint<ro.Number>.Hint(), () =>
+                Then = Core.tMetaFunction(RHint<ro.Number>.Hint(), () => { return 0.tFixed(); }),
+                Else = Core.tMetaFunction(RHint<ro.Number>.Hint(), () =>
                 {
-                    return CoreT.tSubEnvironment(RHint<ro.Number>.Hint(), new()
+                    return Core.tSubEnvironment(RHint<ro.Number>.Hint(), new()
                     {
-                        Environment = pool.tRef().tIO_SelectOne().tAsVariable(out var selection).tYield(),
+                        Environment = pool.tRef().tIOSelectOne().tAsVariable(out var selection).tYield(),
                         SubToken = selfFunc.tRef().tExecuteWith(new()
                         {
                             A = counter.tRef().tAdd(1.tFixed()),
@@ -92,28 +91,18 @@ public class Tester
             A = 0.tFixed(),
             B = 1.Sequence(x => x + 3).Take(5).Map(x => x.tFixed()).tToMulti()
         });
-        var token_test_1 = token_tutorial_2.tIO_SelectMany(Iter.Over(1, 2, 3, 4).Map(x => x.tFixed()).tToConstMulti().tIO_SelectOne());
+        var token_test_1 = token_tutorial_2.tIOSelectMany(Iter.Over(1, 2, 3, 4).Map(x => x.tFixed()).t_ToConstMulti().tIOSelectOne());
         var token_test_2 = token_tutorial_1.tAdd(1.tFixed());
-        var token_test_3 = new t.Fixed<ro.Board.Unit>((new(0) { HP = 3, Owner = new(1), Position = new() { R = 1, U = 2, D = 3 } })).tGetComponent(AxiomT.tEffectSlowCI());
-        var token_test_4 = (new ro.Board.Unit(0) { HP = 3, Owner = new(1), Position = new() { R = 1, U = 2, D = 3 } })
-            .WithComponents(new a.Components.Unit.Effects.Slow.Component().Yield())
-            .tFixed()
-            .tGetComponent(AxiomT.tEffectSlowCI());
-        var token_test_5 = CoreT.tMetaFunction(RHint<ro.Number, ro.Number, ro.Number>.Hint(), (a, b) => a.tRef().tMultiply(b.tRef())).tExecuteWith(new()
+        var token_test_3 = Core.tMetaFunction(RHint<ro.Number, ro.Number, ro.Number>.Hint(), (a, b) => a.tRef().tMultiply(b.tRef())).tExecuteWith(new()
         {
             A = token_tutorial_3,
             B = token_complicated
         });
-        var token_test_6 = token_tutorial_2.tMap(x => x.tRef().tMultiply(2.tFixed()));
-        var token_tester = token_test_6;
-        var rule_test = CoreP.RuleFor<t.Number.Add, ro.Number>(P => P.pOriginalA().pAdd(P.pOriginalB().pAdd(1.tFixed().pDirect(P))));
+        var token_test_4 = token_tutorial_2.tMap(x => x.tRef().tMultiply(2.tFixed()));
+        var token_tester = token_test_4;
+        var rule_test = ProxyStatement.BuildAsRule<t.Number.Add, ro.Number>(P => P.pOriginalA().pAdd(P.pOriginalB().pAdd(1.tFixed().pDirect(P))));
 
-        var startState = new FourZeroOne.IState()
-        {
-            Rules = new() { Elements = [] },
-            Variables = new() { Elements = [] },
-            Board = new() { }
-        };
+        FourZeroOne.IState startState = null;
         _runtime = new FourZeroOne.Runtimes.FrameSaving.Gebug(startState, token_tester);
         var o = await _runtime.Run();
         Console.WriteLine($"FINAL: {o}");
