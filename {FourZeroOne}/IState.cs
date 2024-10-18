5bd287afa888664b6150e8165aee40dd2453ad5c
GFunction <ryan.mm.taylor@gmail.com>
2024-10-18 09:18:46 -0700

    Update IState.cs

--- {FourZeroOne}/IState.cs
+++ {FourZeroOne}/IState.cs
@@ -10,14 +10,16 @@ namespace FourZeroOne
     
     public interface IState
     {
-        public IEnumerable<(Resolution.Unsafe.IStateAddress address, ResObj obj)> Objects { get; }
+        public IEnumerable<(Resolution.Unsafe.IStateAddress, ResObj)> Objects { get; }
         public IEnumerable<Rule.IRule> Rules { get; }
         public IOption<R> GetObject<R>(Resolution.IStateAddress<R> address) where R : class, ResObj;
         public IState WithRules(IEnumerable<Rule.IRule> rules);
-        public IState WithObjects<R>(IEnumerable<(Resolution.IStateAddress<R> address, R obj)> insertions) where R : class, ResObj;
-        public IState WithObjectsUnsafe(IEnumerable<(Resolution.Unsafe.IStateAddress address, ResObj obj)> insertions);
+        public IState WithObjects<R>(IEnumerable<(Resolution.IStateAddress<R>, R)> insertions) where R : class, ResObj;
+        public IState WithObjectsUnsafe(IEnumerable<(Resolution.Unsafe.IStateAddress, ResObj)> insertions);
         public IState WithClearedAddresses(IEnumerable<Resolution.Unsafe.IStateAddress> removals);
     }
+
+    // DEV - Perhaps have a "drivers" folder that has implementations for things like state, IO, etc. perhaps rethink the runtime model, splitting it into components that have drivers.
 
     public static class _Extensions
     {
