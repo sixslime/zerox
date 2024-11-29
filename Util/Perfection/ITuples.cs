e38abd72a38816ac6e25e253f0e452e5b6b93f88
GFunction <ryan.mm.taylor@gmail.com>
2024-11-29 13:02:32 -0800

    Update ITuples.cs

--- Util/Perfection/ITuples.cs
+++ Util/Perfection/ITuples.cs
@@ -16,26 +16,37 @@ namespace Perfection
         public T2 Item2 { get; }
         public T3 Item3 { get; }
     }
-    public record ReadonlyTuple<T1, T2> : ITuple<T1, T2>
+    public record ITupleWrapper<T1, T2> : ITuple<T1, T2>
     {
-        public required T1 Item1 { get; init; }
-        public required T2 Item2 { get; init; }
+        public ITupleWrapper((T1, T2) tuple)
+        {
+            Unwrapped = tuple;
+        }
+        public readonly (T1, T2) Unwrapped;
+        public T1 Item1 => Unwrapped.Item1;
+        public T2 Item2 => Unwrapped.Item2;
+
     }
-    public record ReadonlyTuple<T1, T2, T3> : ITuple<T1, T2, T3>
+    public record ITupleWrapper<T1, T2, T3> : ITuple<T1, T2, T3>
     {
-        public required T1 Item1 { get; init; }
-        public required T2 Item2 { get; init; }
-        public required T3 Item3 { get; init; }
+        public ITupleWrapper((T1, T2, T3) tuple)
+        {
+            Unwrapped = tuple;
+        }
+        public readonly (T1, T2, T3) Unwrapped;
+        public T1 Item1 => Unwrapped.Item1;
+        public T2 Item2 => Unwrapped.Item2;
+        public T3 Item3 => Unwrapped.Item3;
     }
     public static class _Extensions
     {
-        public static ITuple<T1, T2> AsITuple<T1, T2>(this Tuple<T1, T2> tuple)
+        public static ITupleWrapper<T1, T2> AsITuple<T1, T2>(this (T1, T2) tuple)
         {
-            return new ReadonlyTuple<T1, T2>() { Item1 = tuple.Item1, Item2 = tuple.Item2 };
+            return new(tuple);
         }
-        public static ITuple<T1, T2, T3> AsITuple<T1, T2, T3>(this Tuple<T1, T2, T3> tuple)
+        public static ITupleWrapper<T1, T2, T3> AsITuple<T1, T2, T3>(this (T1, T2, T3) tuple)
         {
-            return new ReadonlyTuple<T1, T2, T3>() { Item1 = tuple.Item1, Item2 = tuple.Item2, Item3 = tuple.Item3};
+            return new(tuple);
         }
     }
 }
