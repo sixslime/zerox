3999936018327dacd7767180213ce643f0f4cd30
GFunction <ryan.mm.taylor@gmail.com>
2024-12-02 23:58:11 -0800

    the spec

--- Runner.cs
+++ Runner.cs
@@ -18,6 +18,41 @@ namespace PROTO_ZeroxFour_1
             //game = new MyGameClass(GameSettings.StretchMode, WindowSettings.Default);
             //game.Run();
             await tester.Run();
+            /* DESIRED TESTING EXAMPLE
+             * var tester = new Tester(testableRuntime, baseState);
+             * tester.AddTest(testContext => new()
+             * {
+             *      Name = "basic_addition" // auto-generated if unset
+             *      State = baseState => baseState; // assumed
+             *      Evaluate = 5.tFixed().tAdd(10.tFixed()),
+             *      Selections = [] // optional
+             *      Expect = new() // optional
+             *      {
+             *          Resolution = 15.tFixed(),
+             *          State = prevState => prevState,
+             *      }
+             *      Assert = new() // optional
+             *      {
+             *          Token = x => x is IToken<ro.Number>;
+             *          Resolution = x => x is ResObj;
+             *          State = x => x is IState;
+             *      }
+             * }).UseAs(out var test_1);
+             * 
+             * tester.AddTest(testContext => new()
+             * {
+             *      
+             *      Name = "use_example"
+             *      // this test will be skipped if 'test_1' fails because this test uses 'test_1'
+             *      Evaluate = test_1.Use(testContext).Token.tSubtract(1.tFixed()); //this re-uses the previous test's "Evaluate" token.
+             *      Selections = [] // if 'test_1' had selections, they should appropriately be inserted internally.
+             *      Expect = new()
+             *      {
+             *          Resolution = test_1.Use(testContext).Resolution.Mut(x => x with { Value = x.Value - 1 }); // this should be valid.
+             *      }
+             *      
+             * }
+             */
         }
 
     }
