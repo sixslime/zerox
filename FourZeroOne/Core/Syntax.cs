57623c1105a45c23dfcb466dc36ad7bb82563b24
GFunction <ryan.mm.taylor@gmail.com>
2025-01-20 18:22:46 -0800

    some syntax

--- FourZeroOne/Core/Syntax.cs
+++ FourZeroOne/Core/Syntax.cs
@@ -588,25 +588,25 @@ namespace FourZeroOne.Core.Syntax
         public static p.Direct<TOrig, R> pDirect<TOrig, R>(this Token.IToken<R> token, OriginalHint<TOrig> _) where TOrig : IToken where R : class, ResObj
         { return new(token); }
         public static p.This<TOrig, R> pThis<TOrig, R>(this IOriginalHint<TOrig, IToken<R>> _) where TOrig : IToken<R> where R : class, ResObj
-        { return new([]); }
+        { return new(); }
         public static p.ThisFunction<TOrig, RArg1, ROut> pThisWith<TOrig, RArg1, ROut>(this IOriginalHint<TOrig, IFunction<RArg1, ROut>> _, Structure.Proxy.Args<TOrig, RArg1> args)
             where TOrig : IFunction<RArg1, ROut>
             where RArg1 : class, ResObj
             where ROut : class, ResObj
-        { return new(args.A, []); }
+        { return new(args.A); }
         public static p.ThisFunction<TOrig, RArg1, RArg2, ROut> pThisWith<TOrig, RArg1, RArg2, ROut>(this IOriginalHint<TOrig, IFunction<RArg1, RArg2, ROut>> _, Structure.Proxy.Args<TOrig, RArg1, RArg2> args)
             where TOrig : IFunction<RArg1, RArg2, ROut>
             where RArg1 : class, ResObj
             where RArg2 : class, ResObj
             where ROut : class, ResObj
-        { return new(args.A, args.B, []); }
+        { return new(args.A, args.B); }
         public static p.ThisFunction<TOrig, RArg1, RArg2, RArg3, ROut> pThisWith<TOrig, RArg1, RArg2, RArg3, ROut>(this IOriginalHint<TOrig, IFunction<RArg1, RArg2, RArg3, ROut>> _, Structure.Proxy.Args<TOrig, RArg1, RArg2, RArg3> args)
             where TOrig : IFunction<RArg1, RArg2, RArg3, ROut>
             where RArg1 : class, ResObj
             where RArg2 : class, ResObj
             where RArg3 : class, ResObj
             where ROut : class, ResObj
-        { return new(args.A, args.B, args.C, []); }
+        { return new(args.A, args.B, args.C); }
         public static p.OriginalArg1<TOrig, R> pOriginalA<TOrig, R>(this IOriginalHint<TOrig, Token.Unsafe.IHasArg1<R>> _) where TOrig : Token.Unsafe.IHasArg1<R> where R : class, ResObj
         { return new(); }
         public static p.OriginalArg2<TOrig, R> pOriginalB<TOrig, R>(this IOriginalHint<TOrig, Token.Unsafe.IHasArg2<R>> _) where TOrig : Token.Unsafe.IHasArg2<R> where R : class, ResObj
@@ -614,13 +614,13 @@ namespace FourZeroOne.Core.Syntax
         public static p.OriginalArg3<TOrig, R> pOriginalC<TOrig, R>(this IOriginalHint<TOrig, Token.Unsafe.IHasArg3<R>> _) where TOrig : Token.Unsafe.IHasArg3<R> where R : class, ResObj
         { return new(); }
         public static t.Fixed<r.Multi<R>> t_ToConstMulti<R>(this IEnumerable<t.Fixed<R>> values) where R : class, ResObj
-        { return new(new() { ValueSequence = values.Map(x => x.Resolution) }); }
+        { return new(new() { Values = values.Map(x => x.Resolution).ToPSequence() }); }
 
-        public static Some<ro.Number> rAsRes(this int v) => new(new() { Value = v });
-        public static Some<ro.Bool> rAsRes(this bool v) => new(new() { IsTrue = v });
-        public static Some<r.Multi<R>> rAsRes<R>(this IEnumerable<IOption<R>> v) where R : class, ResObj
+        public static IOption<ro.Number> rAsRes(this int v) => new ro.Number() { Value = v }.AsSome();
+        public static IOption<ro.Bool> rAsRes(this bool v) => new ro.Bool() { IsTrue = v }.AsSome();
+        public static IOption<r.Multi<R>> rAsRes<R>(this IEnumerable<IOption<R>> v) where R : class, ResObj
         {
-            return new(new() { ValueSequence = v.FilterMap(x => x) });
+            return new r.Multi<R>() { Values = v.FilterMap(x => x).ToPSequence() }.AsSome();
         }
     }
 }
