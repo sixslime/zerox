4441aad2ef39d286ecbf219e9b3e4e3725bae69f
GFunction <ryan.mm.taylor@gmail.com>
2025-03-14 23:06:06 -0700

    Create Rodai.cs

---
+++ src/SixShaded.FourZeroOne/Roggi/Defined/Rodai.cs
@@ -0,0 +1,7 @@
+﻿namespace SixShaded.FourZeroOne.Roggi.Defined;
+
+public sealed class Rodai<R>(Axodu axodu, string identifier) : Axovendu(axodu, identifier), IRoda<R>
+    where R : class, Rog
+{
+    protected override string TypeExpression { get; } = $"Rodai<{typeof(R).Name}>";
+}
