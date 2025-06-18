8fb463997e12928b908458e6e734f08ffbaf573a
SixSlime <ryan.mm.taylor@gmail.com>
2025-06-17 21:17:04 -0700

    Update VeilCreationHandleImpl.cs

--- src/SixShaded.VeiledOhOne/Impl/VeilCreationHandleImpl.cs
+++ src/SixShaded.VeiledOhOne/Impl/VeilCreationHandleImpl.cs
@@ -4,14 +4,48 @@ using FourZeroOne.Core.Roggis;
 
 internal class VeilCreationHandleImpl : IVeilCreationHandle
 {
+    public Dictionary<Kor, VeilObservers> KorssaVeils { get; private set; }
+    public Dictionary<Addr, VeilObservers> RodaVeils { get; private set; }
     internal VeilCreationHandleImpl()
     {
-
+        KorssaVeils = new();
+        RodaVeils = new();
     }
-    public void SetKorssaVeil(Kor korssa, EVeilOperation operation, int[] observers) => throw new NotImplementedException();
-    public void SetKorssaVeil(Kor korssa, EVeilOperation operation, IKorssa<IMulti<Number>> observers) => throw new NotImplementedException();
-    public void SetMemoryVeil(Addr roda, EVeilOperation operation, int[] observers) => throw new NotImplementedException();
-    public void SetMemoryVeil(Addr roda, EVeilOperation operation, IKorssa<IMulti<Number>> observers) => throw new NotImplementedException();
+    public void SetKorssaVeil(Kor korssa, EVeilOperation operation, int[] observers)
+    {
+        KorssaVeils[korssa] =
+            new()
+            {
+                Operation = operation,
+                Observers = observers.AsOk(Hint<IKorssa<IMulti<Number>>>.HINT),
+            };
+    }
+    public void SetKorssaVeil(Kor korssa, EVeilOperation operation, IKorssa<IMulti<Number>> observers)
+    {
+        KorssaVeils[korssa] =
+            new()
+            {
+                Operation = operation,
+                Observers = observers.AsErr(Hint<int[]>.HINT),
+            };
+    }
+    public void SetMemoryVeil(Addr roda, EVeilOperation operation, int[] observers)
+    {
+        RodaVeils[roda] =
+            new()
+            {
+                Operation = operation,
+                Observers = observers.AsOk(Hint<IKorssa<IMulti<Number>>>.HINT),
+            };
+    }
+    public void SetMemoryVeil(Addr roda, EVeilOperation operation, IKorssa<IMulti<Number>> observers)
+    {
+        RodaVeils[roda] =
+            new()
+            {
+                Operation = operation,
+                Observers = observers.AsErr(Hint<int[]>.HINT),
+            };
+    }
 
-    internal 
 }
