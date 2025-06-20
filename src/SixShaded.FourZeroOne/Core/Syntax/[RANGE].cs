95399f41567a6ff58ccca06b00baf64824ea1b1a
SixSlime <ryan.mm.taylor@gmail.com>
2025-06-20 12:26:49 -0700

    Create [RANGE].cs

---
+++ src/SixShaded.FourZeroOne/Core/Syntax/[RANGE].cs
@@ -0,0 +1,11 @@
+﻿namespace SixShaded.FourZeroOne.Core.Syntax;
+
+using Roggis;
+
+public static partial class KorssaSyntax
+{
+    public static Korssas.Range.Create kRangeForwardTo(this IKorssa<Number> start, IKorssa<Number> end) => new(start, end);
+    public static Korssas.Range.Create kRangeBackwardTo(this IKorssa<Number> end, IKorssa<Number> start) => new(start, end);
+    public static Korssas.Range.Get.Start kStart(this IKorssa<NumRange> range) => new(range);
+    public static Korssas.Range.Get.End kEnd(this IKorssa<NumRange> range) => new(range);
+}
