8050bce04e6958700417ee627f02c9fceb6526a5
SixSlime <ryan.mm.taylor@gmail.com>
2025-06-20 20:26:21 -0700

    assign now assigns nolla

--- src/SixShaded.FourZeroOne/Core/Korssas/Memory/Object/Insert.cs
+++ src/SixShaded.FourZeroOne/Core/Korssas/Memory/Object/Insert.cs
@@ -2,18 +2,21 @@
 
 using Roveggi;
 
-public sealed record Insert<R> : Korssa.Defined.PureFunction<IRoveggi<IRovedantu<R>>, R, Roggis.Instructions.Assign<R>>
+public sealed record Insert<R> : Korssa.Defined.Function<IRoveggi<IRovedantu<R>>, R, Roggis.Instructions.Assign<R>>
     where R : class, Rog
 {
     public Insert(IKorssa<IRoveggi<IRovedantu<R>>> address, IKorssa<R> obj) : base(address, obj)
     { }
 
-    protected override Roggis.Instructions.Assign<R> EvaluatePure(IRoveggi<IRovedantu<R>> in1, R in2) =>
-        new()
-        {
-            Address = in1.MemWrapped(),
-            Subject = in2,
-        };
+    protected override ITask<IOption<Roggis.Instructions.Assign<R>>> Evaluate(IKorssaContext _, IOption<IRoveggi<IRovedantu<R>>> in1, IOption<R> in2) =>
+        in1.RemapAs(
+            address =>
+                new Roggis.Instructions.Assign<R>()
+                {
+                    Address = address.MemWrapped(),
+                    Subject = in2,
+                })
+            .ToCompletedITask();
 
     protected override IOption<string> CustomToString() => $"{Arg1} <== {Arg2}".AsSome();
 }
--- src/SixShaded.FourZeroOne/Core/Roggis/Instructions/Assign.cs
+++ src/SixShaded.FourZeroOne/Core/Roggis/Instructions/Assign.cs
@@ -4,7 +4,11 @@ public sealed record Assign<D> : Roggi.Defined.Instruction
     where D : class, Rog
 {
     public required IRoda<D> Address { get; init; }
-    public required D Subject { get; init; }
-    public override IMemory TransformMemory(IMemory previousState) => previousState.WithObjects([(Address, Subject).Tiple()]);
+    public required IOption<D> Subject { get; init; }
+
+    public override IMemory TransformMemory(IMemory previousState) =>
+        Subject.Check(out var data)
+            ? previousState.WithObjects([(Address, data).Tiple()])
+            : previousState.WithClearedAddresses([Address]);
     public override string ToString() => $"{Address}<-{Subject}";
 }
