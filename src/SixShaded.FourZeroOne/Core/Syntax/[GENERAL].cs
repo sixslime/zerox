2e6a995b45e6befa25eabe0778e0fb5fb0328768
SixSlime <ryan.mm.taylor@gmail.com>
2025-06-24 21:36:40 -0700

    equal

---
+++ src/SixShaded.FourZeroOne/Core/Syntax/[GENERAL].cs
@@ -0,0 +1,9 @@
+﻿namespace SixShaded.FourZeroOne.Core.Syntax;
+
+using Roggis;
+using Korvessa.Defined;
+
+public static partial class KorssaSyntax
+{
+    public static Korssas.IsEqual kEquals(this Kor a, Kor b) => new(a, b);
+}
