92d197eaff280df36a6b2557d168c9137fa61d7d
SixSlime <ryan.mm.taylor@gmail.com>
2025-06-24 21:21:02 -0700

    ok

---
+++ src/SixShaded.FourZeroOne/Core/Korssas/Equals.cs
@@ -0,0 +1,16 @@
+﻿namespace SixShaded.FourZeroOne.Core.Korssas;
+
+
+public sealed record Equals : Korssa.Defined.PureFunction<Rog, Rog, Roggis.Bool>
+{
+    public Equals(Kor a, Kor b) : base(a, b)
+    { }
+
+    protected override Roggis.Bool EvaluatePure(Rog in1, Rog in2)
+    {
+        return new()
+        {
+            IsTrue = in1.Equals(in2)
+        };
+    }
+}
