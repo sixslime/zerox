cb982857a01abc6db844bb09e56d3c42a6b8561c
SixSlime <ryan.mm.taylor@gmail.com>
2025-06-29 16:51:26 -0700

    Update uSourcedAbility.cs

--- SixShaded.FourZeroOne.Axois.Infinite/Rovetus/Constructs/Ability/uUnsourcedAbility.cs
+++ SixShaded.FourZeroOne.Axois.Infinite/Rovetus/Constructs/Ability/uUnsourcedAbility.cs
@@ -3,4 +3,6 @@
 public interface uUnsourcedAbility : IRovetu, uAbility
 {
     public static readonly Rovu<uSourcedAbility, IRoveggi<Types.uUnsourcedType>> TYPE = new(Axoi.Du, "type");
+    public static readonly Rovu<uSourcedAbility, MetaFunction<Rog>> ACTION = new(Axoi.Du, "action");
+
 }
