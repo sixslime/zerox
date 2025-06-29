104b58f44a7b2422e8edf38863ed77117d166c85
SixSlime <ryan.mm.taylor@gmail.com>
2025-06-29 12:14:44 -0700

    Update [GENERAL].cs

--- src/SixShaded.FourZeroOne/Core/Syntax/[GENERAL].cs
+++ src/SixShaded.FourZeroOne/Core/Syntax/[GENERAL].cs
@@ -6,4 +6,8 @@ using Korvessa.Defined;
 public static partial class KorssaSyntax
 {
     public static Korssas.IsEqual kEquals(this Kor a, Kor b) => new(a, b);
+
+    public static Korssas.TryCast<R> kTryCast<R>(this Kor obj)
+        where R : class, Rog =>
+        new(obj);
 }
