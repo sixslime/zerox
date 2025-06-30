813c251d03380b9ea7a7f237e29f310bcec6d0dd
SixSlime <ryan.mm.taylor@gmail.com>
2025-06-29 17:01:15 -0700

    Update uSourcedAbility.cs

--- SixShaded.FourZeroOne.Axois.Infinite/Rovetus/Constructs/Ability/uSourcedAbility.cs
+++ SixShaded.FourZeroOne.Axois.Infinite/Rovetus/Constructs/Ability/uSourcedAbility.cs
@@ -4,7 +4,11 @@ public interface uSourcedAbility : IRovetu, uAbility
 {
     public static readonly Rovu<uSourcedAbility, IRoveggi<Types.uSourcedType>> TYPE = new(Axoi.Du, "type");
     public static readonly Rovu<uSourcedAbility, IMulti<IRoveggi<uRelativeCoordinate>>> HIT_AREA = new(Axoi.Du, "hit_area");
-    public static readonly Rovu<uSourcedAbility, MetaFunction<IRoveggi<Identifiers.uUnitIdentifier>, IMulti<IRoveggi<Identifiers.uUnitIdentifier>>, IMulti<IRoveggi<Identifiers.uUnitIdentifier>>>> ADDITIONAL_TARGETING = new(Axoi.Du, "additional_targeting");
+    /// <summary>
+    /// Inputs are source unit and the available targets within the hit area (accounting for attack/defense).<br></br>
+    /// Output is the final list of valid targets.
+    /// </summary>
+    public static readonly Rovu<uSourcedAbility, MetaFunction<IRoveggi<Identifiers.uUnitIdentifier>, IMulti<IRoveggi<Identifiers.uUnitIdentifier>>, IMulti<IRoveggi<Identifiers.uUnitIdentifier>>>> POST_TARGETING = new(Axoi.Du, "post_targeting");
     public static readonly Rovu<uSourcedAbility, IMulti<IRoveggi<UnitEffects.uUnitEffect>>> EFFECTS = new(Axoi.Du, "effects");
     public static readonly Rovu<uSourcedAbility, MetaFunction<IRoveggi<Identifiers.uUnitIdentifier>, IRoveggi<Identifiers.uUnitIdentifier>, Rog>> FOLLOWUP = new(Axoi.Du, "followup");
 }
