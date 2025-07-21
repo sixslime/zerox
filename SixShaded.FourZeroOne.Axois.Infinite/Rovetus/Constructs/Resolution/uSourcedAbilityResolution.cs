245dcd3e65c27b3312ff6b8dbdcbfda053a3681c
SixSlime <114969407+sixslime@users.noreply.github.com>
2025-07-20 22:23:39 -0700

    what

---
+++ SixShaded.FourZeroOne.Axois.Infinite/Rovetus/Constructs/Resolution/uSourcedAbilityResolution.cs
@@ -0,0 +1,8 @@
+﻿namespace SixShaded.FourZeroOne.Axois.Infinite.Rovetus.Constructs.Resolution;
+
+public interface uSourcedAbilityResolution : IRovetu
+{
+    public static readonly Rovu<uSourcedAbilityResolution, IRoveggi<Ability.uSourcedAbility>> ABILITY = new(Axoi.Du, "ability");
+    public static readonly Rovu<uSourcedAbilityResolution, IRoveggi<Identifier.uUnitIdentifier>> SOURCE = new(Axoi.Du, "source");
+    public static readonly Rovu<uSourcedAbilityResolution, IRoveggi<Identifier.uUnitIdentifier>> TARGET = new(Axoi.Du, "target");
+}
