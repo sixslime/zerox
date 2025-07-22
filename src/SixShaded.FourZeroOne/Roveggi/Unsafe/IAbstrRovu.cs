361ae7a9333761f4caf79a015912ca95b8be27c4
SixSlime <114969407+sixslime@users.noreply.github.com>
2025-07-22 16:43:44 -0700

    bruh

---
+++ src/SixShaded.FourZeroOne/Roveggi/Unsafe/IAbstrRovu.cs
@@ -0,0 +1,9 @@
+﻿namespace SixShaded.FourZeroOne.Roveggi.Unsafe;
+
+// DEV:
+//  may not be needed
+public interface IAbstrRovu<in C>
+    where C : IRovetu
+{
+
+}
---
+++ src/SixShaded.FourZeroOne/Roveggi/Unsafe/IGetRovu.cs
@@ -0,0 +1,6 @@
+namespace SixShaded.FourZeroOne.Roveggi.Unsafe;
+
+public interface IGetRovu<in C, R>
+    where C : IRovetu
+    where R : class, Rog
+{ }
