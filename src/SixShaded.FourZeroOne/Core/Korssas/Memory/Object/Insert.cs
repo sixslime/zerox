a342593ece5a8493a54e0f9876dedd08f5b492ed
SixSlime <114969407+sixslime@users.noreply.github.com>
2025-07-24 15:17:32 -0700

    removed korssas

--- src/SixShaded.FourZeroOne/Core/Korssas/Memory/Object/Insert.cs
+++ src/SixShaded.FourZeroOne/Core/Korssas/Memory/Object/Insert.cs
@@ -2,18 +2,21 @@
 
 using Roveggi;
 
-public sealed record Insert<R> : Korssa.Defined.PureFunction<IRoveggi<Rovedantu<R>>, R, Roggis.Instructions.Assign<R>>
+public sealed record Insert<R> : Korssa.Defined.Function<IRoveggi<Rovedantu<R>>, R, Roggis.Instructions.Assign<R>>
     where R : class, Rog
 {
     public Insert(IKorssa<IRoveggi<Rovedantu<R>>> address, IKorssa<R> obj) : base(address, obj)
     { }
 
-    protected override Roggis.Instructions.Assign<R> EvaluatePure(IRoveggi<Rovedantu<R>> in1, R in2) =>
-        new()
-        {
-            Address = in1.Rovedanggi(),
-            Subject = in2,
-        };
+    protected override ITask<IOption<Roggis.Instructions.Assign<R>>> Evaluate(IKorssaContext runtime, IOption<IRoveggi<Rovedantu<R>>> in1, IOption<R> in2) =>
+        in1.RemapAs(
+            addressObject =>
+                new Roggis.Instructions.Assign<R>
+                {
+                    Address = addressObject.Rovedanggi(),
+                    Data = in2
+                })
+            .ToCompletedITask();
 
     protected override IOption<string> CustomToString() => $"{Arg1} <== {Arg2}".AsSome();
 }
--- src/SixShaded.FourZeroOne/Core/Roggis/Instructions/Redact.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SixShaded.FourZeroOne.Core.Roggis.Instructions;
-
-public sealed record Redact : Roggi.Defined.Instruction
-{
-    public required Addr Address { get; init; }
-    public override IMemory TransformMemory(IMemory context) => context.WithClearedAddresses([Address]);
-}
