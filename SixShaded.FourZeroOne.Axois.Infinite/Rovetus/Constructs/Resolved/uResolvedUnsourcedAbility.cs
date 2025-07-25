c8d63ff92fc34d7d1911cfa7d2de956005f84f15
SixSlime <114969407+sixslime@users.noreply.github.com>
2025-07-24 19:48:26 -0700

    bruh

--- SixShaded.FourZeroOne.Axois.Infinite/Rovetus/Constructs/Resolved/uResolvedUnsourcedAbility.cs
+++ SixShaded.FourZeroOne.Axois.Infinite/Rovetus/Constructs/Resolved/uResolvedUnsourcedAbility.cs
@@ -3,13 +3,4 @@
 public interface uResolvedUnsourcedAbility : uResolved, IConcreteRovetu
 {
     public static readonly Rovu<uResolvedUnsourcedAbility, IRoveggi<Ability.uUnsourcedAbility>> ABILITY = new(Axoi.Du, "ability");
-    public static readonly ImplementationStatement<uResolvedUnsourcedAbility> __IMPLEMENTS =
-        c =>
-            c.ImplementGet(
-            INSTRUCTIONS,
-            iSelf =>
-                iSelf.kRef()
-                    .kGetRovi(ABILITY)
-                    .kGetRovi(Ability.uUnsourcedAbility.ACTION)
-                    .kExecute());
 }
