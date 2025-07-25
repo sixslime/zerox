9c8c42c4b9767fcf1965b0b0c01ccde68ec39b9d
SixSlime <114969407+sixslime@users.noreply.github.com>
2025-07-24 19:31:14 -0700

    o

--- SixShaded.FourZeroOne.Axois.Infinite/Syntax/Syntax.cs
+++ SixShaded.FourZeroOne.Axois.Infinite/Syntax/Syntax.cs
@@ -1,13 +1,15 @@
 namespace SixShaded.FourZeroOne.Axois.Infinite.Syntax;
 
 using Rovetus.Identifier;
+using Core = Core.Syntax.Core;
 using U = Rovetus;
 public static partial class Infinite
 {
-    public static IKorssa<IRoveggi<uPlayerIdentifier>> kCurrentPlayer() => throw new NotImplementedException();
+    public static IKorssa<IRoveggi<U.Anchors.uGameAnchor>> Game => Core.kCompose<U.Anchors.uGameAnchor>();
+    public static IKorssa<IRoveggi<U.Anchors.uConfigAnchor>> Configuration => Core.kCompose<U.Anchors.uConfigAnchor>();
 }
 
 public static partial class KorssaSyntax
 {
-    public static Kor kRealize(this IKorssa<IRoveggi<U.Config.uGameConfiguration>> configuration) => throw new NotImplementedException();
+
 }
