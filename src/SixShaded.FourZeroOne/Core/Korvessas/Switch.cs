a7a8dc817d729c7d6ba2cee2994f3a0f14984ce7
SixSlime <114969407+sixslime@users.noreply.github.com>
2025-07-26 20:51:23 -0700

    Create Switch.cs

---
+++ src/SixShaded.FourZeroOne/Core/Korvessas/Switch.cs
@@ -0,0 +1,48 @@
+﻿namespace SixShaded.FourZeroOne.Core.Korvessas;
+
+using Roggis;
+using Korvessa.Defined;
+using Syntax;
+
+public static class Switch<RIn, ROut>
+    where ROut : class, Rog
+    where RIn : class, Rog
+{
+    public static Korvessa<RIn, IMulti<MetaFunction<RIn, Bool>>, IMulti<MetaFunction<ROut>>, ROut> Construct(IKorssa<RIn> input, IKorssa<IMulti<MetaFunction<RIn, Bool>>> matchers, IKorssa<IMulti<MetaFunction<ROut>>> returners) =>
+        new(input, matchers, returners)
+        {
+            Du = Axoi.Korvedu("Switch"),
+            Definition =
+                (_, iInput, iMatchers, iReturners) =>
+                    Core.kMetaFunctionRecursive<Number, ROut>(
+                        [],
+                        (iRecurse, iIndex) =>
+                            iMatchers.kRef()
+                                .kGetIndex(iIndex.kRef())
+                                .kExecuteWith(
+                                new()
+                                {
+                                    A = iInput.kRef()
+                                })
+                                .kIfTrueExplicit<ROut>(
+                                new()
+                                {
+                                    Then = iReturners.kRef().kGetIndex(iIndex.kRef()),
+                                    Else =
+                                        iRecurse.kRef()
+                                            .kExecuteWith(
+                                            new()
+                                            {
+                                                A = iIndex.kRef().kAdd(1.kFixed())
+                                            })
+                                            .kMetaBoxed([])
+                                })
+                                .kExecute())
+                        .kExecuteWith(
+                        new()
+                        {
+                            A = 1.kFixed()
+                        })
+
+        };
+}
