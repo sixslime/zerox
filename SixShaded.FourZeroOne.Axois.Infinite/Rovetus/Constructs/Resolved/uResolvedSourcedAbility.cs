5c999375c0da2d4fa9cbf20feda0cc0e684d4c84
SixSlime <114969407+sixslime@users.noreply.github.com>
2025-07-27 10:09:03 -0700

    bruh

--- SixShaded.FourZeroOne.Axois.Infinite/Rovetus/Constructs/Resolved/uResolvedSourcedAbility.cs
+++ SixShaded.FourZeroOne.Axois.Infinite/Rovetus/Constructs/Resolved/uResolvedSourcedAbility.cs
@@ -2,9 +2,11 @@
 
 using Core = Core.Syntax.Core;
 
-public interface uResolvedSourcedAbility : uResolved, IConcreteRovetu
+public interface uResolvedSourcedAbility : IConcreteRovetu, uResolvedAbility
 {
-    public static readonly Rovu<uResolvedSourcedAbility, IRoveggi<Ability.uSourcedAbility>> ABILITY = new(Axoi.Du, "ability");
+    public static new readonly Rovu<uResolvedSourcedAbility, IRoveggi<Ability.uSourcedAbility>> ABILITY = new(Axoi.Du, "ability");
     public static readonly Rovu<uResolvedSourcedAbility, IRoveggi<Identifier.uUnitIdentifier>> SOURCE = new(Axoi.Du, "source");
     public static readonly Rovu<uResolvedSourcedAbility, IRoveggi<Identifier.uUnitIdentifier>> TARGET = new(Axoi.Du, "target");
+    public static readonly ImplementationStatement<uResolvedSourcedAbility> __IMPLEMENTS =
+        c => c.ImplementGet(uResolvedAbility.ABILITY, iSelf => iSelf.kRef().kGetRovi(ABILITY));
 }
