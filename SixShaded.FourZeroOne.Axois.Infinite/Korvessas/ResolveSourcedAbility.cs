aa493c7303b80733c968b6a036b8a7ff655d9d48
SixSlime <114969407+sixslime@users.noreply.github.com>
2025-08-14 13:24:41 -0700

    ok

--- SixShaded.FourZeroOne.Axois.Infinite/Korvessas/ResolveSourcedAbility.cs
+++ SixShaded.FourZeroOne.Axois.Infinite/Korvessas/ResolveSourcedAbility.cs
@@ -6,20 +6,17 @@ using Core = Core.Syntax.Core;
 
 public static class ResolveSourcedAbility
 {
-    public static Korvessa<IRoveggi<uSourcedAbility>, IRoveggi<uResolvedSourcedAbility>> Construct(IKorssa<IRoveggi<uSourcedAbility>> ability)
-    => new(ability)
-    {
-        Du = Axoi.Korvedu("ResolveSourcedAbility"),
-        Definition = 
-            (_, iAbility) =>
-                Core.kSubEnvironment<IRoveggi<uResolvedSourcedAbility>>(
-                new()
-                {
-                    Environment =
-                        [
-
-                        ],
-                    Value =
-                })
-    }
+    public static Korvessa<IRoveggi<uSourcedAbility>, IRoveggi<uResolvedSourcedAbility>> Construct(IKorssa<IRoveggi<uSourcedAbility>> ability) =>
+        new(ability)
+        {
+            Du = Axoi.Korvedu("ResolveSourcedAbility"),
+            Definition =
+                (_, iAbility) =>
+                    Core.kSubEnvironment<IRoveggi<uResolvedSourcedAbility>>(
+                    new()
+                    {
+                        Environment = [],
+                        Value = Core.kCompose<uResolvedSourcedAbility>()
+                    })
+        };
 }
--- SixShaded.FourZeroOne.Axois.Infinite/Rovetus/Constructs/Ability/uUnsourcedAbility.cs
+++ SixShaded.FourZeroOne.Axois.Infinite/Rovetus/Constructs/Ability/uUnsourcedAbility.cs
@@ -2,7 +2,8 @@
 
 public interface uUnsourcedAbility : IConcreteRovetu, uAbility
 {
-    public static readonly Rovu<uUnsourcedAbility, IRoveggi<Types.uUnsourcedType>> TYPE = new(Axoi.Du, "type");
+    public static new readonly Rovu<uUnsourcedAbility, IRoveggi<Types.uUnsourcedType>> TYPE = new(Axoi.Du, "type");
     public static readonly Rovu<uUnsourcedAbility, MetaFunction<Rog>> ACTION = new(Axoi.Du, "action");
 
+    public static readonly ImplementationStatement<uUnsourcedAbility> __IMPLEMENTS = c => c.ImplementGet(uAbility.TYPE, iSelf => iSelf.kRef().kGetRovi(TYPE));
 }
