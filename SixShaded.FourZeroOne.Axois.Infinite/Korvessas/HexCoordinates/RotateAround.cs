c7ea7a12c316647f05e4873e5ab172b9e470fa85
SixSlime <114969407+sixslime@users.noreply.github.com>
2025-08-15 12:46:16 -0700

    divide and modulo

---
+++ SixShaded.FourZeroOne.Axois.Infinite/Korvessas/HexCoordinates/RotateAround.cs
@@ -0,0 +1,29 @@
+﻿namespace SixShaded.FourZeroOne.Axois.Infinite.Korvessas.HexCoordinates;
+
+using Core = Core.Syntax.Core;
+using HexCoords = IRoveggi<u.Constructs.uHexCoordinates>;
+using HexOffset = IRoveggi<u.Constructs.uHexOffset>;
+using HexOffsetType = u.Constructs.uHexOffset;
+using HexType = u.Constructs.uHexCoordinates;
+
+public static class RotateAround
+{
+    // clockwise
+    public static Korvessa<HexCoords, HexCoords, Number, HexCoords> Construct(IKorssa<HexCoords> coordinate, IKorssa<HexCoords> anchor, IKorssa<Number> rotation) =>
+        new(coordinate, anchor, rotation)
+        {
+            Du = Axoi.Korvedu("HexCoordinates.RotateAround"),
+            Definition =
+                (_, iCoordinate, iAnchor, iRotation) =>
+                    Core.kCompose<HexOffsetType>()
+                        .kWithRovi(HexType.R, 
+                        iA.kRef().kGetRovi(HexType.R)
+                            .kAdd(iB.kRef().kGetRovi(HexType.R)))
+                        .kWithRovi(HexType.U,
+                        iA.kRef().kGetRovi(HexType.U)
+                            .kAdd(iB.kRef().kGetRovi(HexType.U)))
+                        .kWithRovi(HexType.D,
+                        iA.kRef().kGetRovi(HexType.D)
+                            .kAdd(iB.kRef().kGetRovi(HexType.D)))
+        };
+}
--- src/SixShaded.FourZeroOne/Core/Syntax/[NUMBER].cs
+++ src/SixShaded.FourZeroOne/Core/Syntax/[NUMBER].cs
@@ -7,5 +7,7 @@ public static partial class KorssaSyntax
     public static Korssas.Number.Add kAdd(this IKorssa<Number> a, IKorssa<Number> b) => new(a, b);
     public static Korssas.Number.Subtract kSubtract(this IKorssa<Number> a, IKorssa<Number> b) => new(a, b);
     public static Korssas.Number.Multiply kMultiply(this IKorssa<Number> a, IKorssa<Number> b) => new(a, b);
+    public static Korssas.Number.Divide kDivide(this IKorssa<Number> a, IKorssa<Number> b) => new(a, b);
+    public static Korssas.Number.Modulo kModulo(this IKorssa<Number> a, IKorssa<Number> b) => new(a, b);
     public static Korssas.Number.GreaterThan kIsGreaterThan(this IKorssa<Number> a, IKorssa<Number> b) => new(a, b);
 }
