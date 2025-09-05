b68c6f2ce94d10eac0d00902fff24342f07eb85a
SixSlime <ryan.mm.taylor@gmail.com>
2025-09-05 14:29:53 -0700

    coretypematcher

--- src/SixShaded.FZOTypeMatch/Syntax/Extensions.cs
+++ src/SixShaded.FZOTypeMatch/Syntax/Extensions.cs
@@ -1,6 +1,12 @@
 namespace SixShaded.FZOTypeMatch.Syntax;
-
+using FourZeroOne.Roveggi.Unsafe;
 public static class Extensions
 {
-    public static IOption<IKorssaType> FZOTypeMatch(this Kor korssa, FZOTypeMatch match) => match.GetKorssaTypeInfo(korssa);
+    public static KorssaTypeInfo FZOTypeInfo(this Kor korssa, FZOTypeMatch typeMatch) => typeMatch.GetKorssaTypeInfo(korssa);
+    public static RoggiTypeInfo FZOTypeInfo(this Rog roggi, FZOTypeMatch typeMatch) => typeMatch.GetRoggiTypeInfo(roggi);
+    public static RovetuTypeInfo FZOTypeInfo(this IRoveggi<IRovetu> roveggi, FZOTypeMatch typeMatch) => typeMatch.GetRovetuTypeInfo(roveggi);
+    public static RovuInfo FZOTypeInfo(this IRovu rovu, FZOTypeMatch typeMatch) => typeMatch.GetRovuInfo(rovu);
+    public static AbstractRovuInfo FZOTypeInfo(this IAbstractRovu rovu, FZOTypeMatch typeMatch) => typeMatch.GetRovuInfo(rovu);
+    public static VarovuInfo FZOTypeInfo(this IVarovu varovu, FZOTypeMatch typeMatch) => typeMatch.GetVarovuInfo(varovu);
+    public static IOption<IFZOTypeInfo<IFZOType>> TryGetFZOTypeInfo(this Type type, FZOTypeMatch typeMatch) => typeMatch.GetFZOTypeInfoDynamic(type);
 }
