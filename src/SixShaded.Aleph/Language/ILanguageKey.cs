69504ff19cb34d928b287c4035afb7ac1ada8660
SixSlime <ryan.mm.taylor@gmail.com>
2025-10-12 13:05:19 -0700

    bruh

---
+++ src/SixShaded.Aleph/Language/ILanguageKey.cs
@@ -0,0 +1,16 @@
+﻿namespace SixShaded.Aleph.Language;
+using FZOTypeMatch;
+using TranslationBuilders;
+
+// LEFTOFF:
+// need way to recursively translate; e.g. for metafunction display
+// probably need special return type
+// builder pattern, similar to StringBuilder
+public interface ILanguageKey
+{
+    public IOption<KorssaTranslation> TranslateKorssa(IKorssaType type, KorssaTranslationBuilder builder);
+    public string TranslateRoggi(IRoggiType type);
+    public string TranslateRovetu(IRovetuType type);
+    public string TranslateRovu(FourZeroOne.Roveggi.Unsafe.IRovu rovu);
+    public string TranslateRoda(Addr roda);
+}
---
+++ src/SixShaded.Aleph/Language/LanguageProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace SixShaded.Aleph.Language;
+using FZOTypeMatch;
+using CoreTypeMatcher;
+public record LanguageProvider
+{
+    public required FZOTypeMatch TypeMatch { get; init; }
+    public required ILanguageKey Key { get; init; }
+}
