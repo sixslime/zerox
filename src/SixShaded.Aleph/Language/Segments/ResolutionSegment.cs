3dd9b6ba25a465d22c7380fd11e63352ab90714b
SixSlime <ryan.mm.taylor@gmail.com>
2025-10-15 17:41:45 -0700

    q

--- src/SixShaded.Aleph/Language/Segments/ResolutionSegment.cs
+++ src/SixShaded.Aleph/Language/Segments/ResolutionSegment.cs
@@ -2,5 +2,5 @@ namespace SixShaded.Aleph.Language.Segments;
 
 internal class ResolutionSegment<T> : ITranslationResolution
 {
-    public required Func<T, TranslationBuilder, Translation> Expression { get; init; }
+    public required Func<T, string> Expression { get; init; }
 }
